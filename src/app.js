1de43691ba223323a64b4dad079d0e68c63c9886
BHDHakim <hakim.bhd@gmail.com>
2022-05-23 14:13:11 +0100

    Fix formatting

--- src/app.js
+++ src/app.js
@@ -1,37 +1,37 @@
-let bookCollection = [
-    {
-        title: "The Sulivan",
-        author: "Mario Puzo"
-    },
-    {
-        title:"The Poison Arrow Tree",
-        author: "Shel Arensen"
-    },
-]
+const bookCollection = [
+  {
+    title: 'The Sulivan',
+    author: 'Mario Puzo',
+  },
+  {
+    title: 'The Poison Arrow Tree',
+    author: 'Shel Arensen',
+  },
+];
 
 function addBook(book) {
-    bookCollection.push(book);
+  bookCollection.push(book);
 }
 
 function removeBook(index) {
-   bookCollection.splice(index, 1);
+  bookCollection.splice(index, 1);
 }
 
-function displayBooks (books) {
-    const booksContainer = document.querySelector("#book-collection")
+function displayBooks(books) {
+  const booksContainer = document.querySelector('#book-collection');
 
-    const booksHtml = books.map(({title, author}) => {
-        return `
+  const booksHtml = books.map(
+    ({ title, author }) => `
             <div>
                 <h2>${title}</h2>
                 <p>${author}</p>
                 <button>Remove</button>
                 <hr />
             <div>
-        `
-    })
+        `,
+  );
 
-    booksContainer.innerHTML = booksHtml.join('');
+  booksContainer.innerHTML = booksHtml.join('');
 }
 
 window.onload = () => displayBooks(bookCollection);
