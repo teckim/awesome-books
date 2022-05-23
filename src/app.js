18233a1130ce45fa9079ae64aa5481cac059798d
BHDHakim <hakim.bhd@gmail.com>
2022-05-23 14:08:14 +0100

    Display book collection

--- src/app.js
+++ src/app.js
@@ -16,3 +16,22 @@ function addBook(book) {
 function removeBook(index) {
    bookCollection.splice(index, 1);
 }
+
+function displayBooks (books) {
+    const booksContainer = document.querySelector("#book-collection")
+
+    const booksHtml = books.map(({title, author}) => {
+        return `
+            <div>
+                <h2>${title}</h2>
+                <p>${author}</p>
+                <button>Remove</button>
+                <hr />
+            <div>
+        `
+    })
+
+    booksContainer.innerHTML = booksHtml.join('');
+}
+
+window.onload = () => displayBooks(bookCollection);
