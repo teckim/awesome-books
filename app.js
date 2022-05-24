8d29ae116088f243b6dd076bea9b355148d7e54d
bruk19 <brukteshome21@gmail.com>
2022-05-24 11:59:14 +0300

    add js funtionalities

--- app.js
+++ app.js
@@ -8,30 +8,55 @@ const bookCollection = [
     author: 'Shel Arensen',
   },
 ];
+/* eslint-disable no-plusplus */
+/* eslint-disable radix */
 
-function addBook(book) {
-  bookCollection.push(book);
+const booksList = document.querySelector('.books-list');
+const bookUniqueId = 'bookStorage';
+
+function renderBook(book) {
+  const bookItem = document.createElement('div');
+  bookItem.id = book.id;
+  bookItem.className = 'bookItem';
+  bookItem.innerHTML = `
+    <h2>${book.title}</h2>
+    <p>${book.author}</p>
+    <button class="btn" type="button" name="button" value="remove">Remove</button>
+  `;
+
+  return bookItem;
 }
 
-function removeBook(index) {
-  bookCollection.splice(index, 1);
+function remove(e) {
+  if (e.target.classList.contains('btn')) {
+    const bookItem = e.target.parentElement;
+    bookItem.style.display = 'none';
+
+    const books = JSON.parse(localStorage.getItem(bookUniqueId));
+    const reducedBooks = books.filter((book) => book.id !== parseInt(bookItem.id));
+    localStorage.setItem(bookUniqueId, JSON.stringify(reducedBooks));
+  }
 }
 
-function displayBooks(books) {
-  const booksContainer = document.querySelector('#book-collection');
+function add(event) {
+  event.preventDefault();
 
-  const booksHtml = books.map(
-    ({ title, author }) => `
-            <div>
-                <h2>${title}</h2>
-                <p>${author}</p>
-                <button>Remove</button>
-                <hr />
-            <div>
-        `,
-  );
+  let idCounter = parseInt(localStorage.getItem('idCounter'));
+  const newBook = {
+    id: ++idCounter,
+    title: this.elements.title.value,
+    author: this.elements.author.value,
+  };
+  booksList.appendChild(renderBook(newBook));
 
-  booksContainer.innerHTML = booksHtml.join('');
+  let books = localStorage.getItem(bookUniqueId);
+  if (books === null) books = [];
+  else books = JSON.parse(books);
+
+  books.push(newBook);
+  localStorage.setItem(bookUniqueId, JSON.stringify(books));
+  localStorage.setItem('idCounter', idCounter);
 }
 
-window.onload = () => displayBooks(bookCollection);
+document.forms[0].addEventListener('submit', add);
+document.body.addEventListener('click', remove);
